b7a55a1d4b4200362c1cb8542ed4c4b4dd20712e
jakobnissen <jakobnybonissen@gmail.com>
2018-05-30 15:15:08 +0200

    New module: Threshold     Estimates optimal clustering threshold
    Clustering:
        Removes spearman correlation as it didn't make things better.
        Replaced the normalizing with the function from vambtools.
        Tandem clustering: Now deduplicates (collapses) the bins whenever a bin is created. This should help memory consumption a little.
        Moved _distances_to_cluster to vambtools to allow threshold.py to import it
        Automatic threshold detection is now possible
        Commandline args inner and outer moved to optional args
    
    Parsebam:
        Corrected some errors in docstrings
    
    Vambtools:
        Added function pearson_distance
    
    Misc. changes to tutorial

--- benchmark.py
+++ benchmark.py
@@ -1,23 +1,35 @@
 
 __doc__ = """Benchmarks observed clusters given a reference.
     
-observed file should be tab-sep lines of: cluster, contig
-reference should be tab-sep lines of: cluster, contig, length
+Contains the objects Reference, Observed and BenchMarkResult.
+
+type help(BenchMarkResult) to see what gets measured.
+
+You can instantiate References and Observed from {cluster: set(contigs)} dicts
+or from files (shown below)
+
+>>> with open(reference_path) as reference_file:
+>>>    reference = Reference.fromfile(reference_file)
+>>>    
+>>> with open(observed_path) as observed_file
+>>>     observed = Observed.fromfile(observed_file, reference)
+>>>
+>>> benchmarkresult = BenchMarkResult(reference=reference, observed=observed)
+
+Observed file should be tab-sep lines of: cluster, contig
+Reference file should be tab-sep lines of: cluster, contig, length
 
 The cluster names need not be the same for obs. and ref. file, but the contig
 names do.
 
-For references without lengths, or if you wish to not weigh contigs
-by length, set length of all contigs in reference to 1.
-
 NOTE: This version counts the number of reference bins detected at the
-different levents of recall and precision."""
+different levents of recall and precision.
+"""
 
 
 
 import sys
 import os
-import argparse
 
 from math import sqrt
 from collections import defaultdict, Counter
@@ -28,12 +40,12 @@ class Reference:
     """Reference clusters.
 
     Init with {name: set_of_contig} dict (a) and {contigname: length} dict (b):
-    Reference(a, b)
+    >>> reference = Reference(a, b)
     
     Init with iterator of tab-sep lines of clustername, contigname, contiglength:
-    with open('/path/to/reference.tsv') as line_iterator:
-        filtered_lines = (line for line in line_iterator if 'human' in line)
-        reference = Reference.fromfile(filtered_lines) # or with filehandle
+    >>> with open('/path/to/reference.tsv') as line_iterator:
+    ...     filtered_lines = filter(str.startswith('HUMAN'), line_iterator)
+    ...     reference = Reference.fromfile(filtered_lines) # or with filehandle
     
     Attributes:
         self.nbins: Number of bins
@@ -58,6 +70,8 @@ class Reference:
                 self.binof[contig] = cluster
                 length = self.contiglength[contig]
                 self.binlength[cluster] = self.binlength.get(cluster, 0) + length
+                
+        self.length = sum(self.binlength.values())
     
     @classmethod
     def fromfile(cls, filehandle):
@@ -97,10 +111,11 @@ class Observed:
     """Observed clusters.
 
     Init with {name: set_of_contig} dict and Reference object (b):
-    Observed(a, b)
+    >>> observed = Observed(a, b)
     
     Init with file of tab-sep lines w. clustername, contigname and Reference:
-    Reference.fromfile(open_filehandle, reference_object)
+    >>> with open('/path/to/observed.tsv') as line_iterator:
+    ...     observed = Observed.fromfile(line_iterator, reference)
     """
     
     def __init__(self, contigsof, reference):
@@ -148,20 +163,20 @@ class Observed:
 
 
 class BenchMarkResult:
-    """An object holding some benchmarkresults:
+    """An object holding some benchmark results:
     
     Init from Reference object and Observed object using keywords:
-    result = BenchmarkResult(reference=reference, observed=observed)
+    >>> result = BenchmarkResult(reference=reference, observed=observed)
     
     recall_weight is the weight of recall relative to precision; to weigh
     precision higher than recall, use a value between 0 and 1.
     
     Get number of references found at recall, precision:
-        result[(recall, precision)]
+    >>> result[(recall, precision)]
     Get number of references found at recall or precision
-        result.atrecall/atprecision(recall/precision)
+    >>> result.atrecall/atprecision(recall/precision)
     Print number of references at all recalls and precisions:
-        result.printmatrix()
+    >>> result.printmatrix()
         
     Attributes:
         self.nreferencebins: Number of reference bins
@@ -179,6 +194,25 @@ class BenchMarkResult:
     _DEFAULTRECALLS = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
     _DEFAULTPRECISIONS = [0.7, 0.8, 0.9, 0.95, 0.99]
     
+    def _iterpairs(self, observed, reference, true_binid):
+        true_contigs = reference.contigsof[true_binid]
+        
+        ref_binsize = reference.binlength[true_binid]
+        
+        obs_bins = {observed.binof.get(contig, None) for contig in true_contigs}
+        obs_bins.discard(None)
+        
+        for obs_bin in obs_bins:
+            obs_binsize = observed.binlength[obs_bin]
+            intersection = observed.contigsof[obs_bin] & true_contigs
+            true_pos = sum(reference.contiglength[i] for i in intersection)
+            true_neg = reference.length + true_pos - ref_binsize - obs_binsize
+            false_pos = obs_binsize - true_pos
+            false_neg = ref_binsize - true_pos
+            
+            yield true_pos, true_neg, false_pos, false_neg
+        
+    
     @staticmethod
     def _mcc(true_pos, true_neg, false_pos, false_neg, sqrt=sqrt):
         mcc_num = true_pos * true_neg - false_pos * false_neg
@@ -189,7 +223,9 @@ class BenchMarkResult:
         return mcc
     
     @staticmethod
-    def _fscore(recall, precision, recall_weight):
+    def _fscore(true_pos, true_neg, false_pos, false_neg, recall_weight):
+        recall = true_pos / (true_pos + false_neg)
+        precision = true_pos / (true_pos + false_pos)
         fscore = (recall_weight*recall_weight + 1) * (precision*recall)
         fscore /= (recall_weight*recall_weight*precision + recall)
         
@@ -215,12 +251,10 @@ class BenchMarkResult:
         
         self._binsfound = Counter()
         
-        referencelength = sum(reference.contiglength.values())
-        
-        for true_binid, true_contigs in reference.contigsof.items():            
+        for true_binid, true_contigs in reference.contigsof.items():
             recalls_precisions = list()
             max_fscore = 0
-            max_mcc = -1
+            max_mcc = -2
             
             obs_bins = {observed.binof.get(contig, None) for contig in true_contigs}
             obs_bins.discard(None)
@@ -248,6 +282,10 @@ class BenchMarkResult:
                 recalls_precisions.append((recall, precision))
                 
             self.fscoreof[true_binid] = max_fscore
+            
+            if max_mcc == -2:
+                max_mcc = 0
+            
             self.mccof[true_binid] = max_mcc
 
             for min_recall in recalls:
@@ -276,29 +314,50 @@ class BenchMarkResult:
         return self._binsfound[key]
     
     def atrecall(self, recall):
+        """Returns a list of counted bins with the different precisions at
+        a given recall.
+        
+        >>> result.atrecall(0.9)
+        [141, 130, 115, 85, 55]
+        """
+        
         if not recall in self.recalls:
             raise ValueError('Not initialized with that recall')
             
         return [self[(recall, p)] for p in self.precisions]
     
     def atprecision(self, precision):
+        """Returns a list of counted bins with the different recalls at
+        a given precision.
+        
+        >>> result.precision(0.9)
+        [141, 130, 115, 85, 55, 30, 14]
+        """
         if not precision in self.precisions:
             raise KeyError('Not initialized with that precision')
             
         return [self[(r, precision)] for r in self.recalls]
     
     def reference_fraction(self, key):
-        """As fraction of existing bins"""
+        """Get number of bins / total ref. bins given a (recall, prec) tuple
+        
+        >>> result.reference_fraction((0.5, 0.9))
+        0.315542
+        """
         
         return self[key] / self.nreferencebins
     
     def observed_fraction(self, key):
-        """As fraction of existing bins"""
+        """Get number of bins / total obs. bins given a (recall, prec) tuple
+        
+        >>> result.observed_fraction((0.5, 0.9))
+        0.291176308742343
+        """
         
         return self[key] / self.nobservedbins
     
     def printmatrix(self):
-        """Prints the result."""
+        """Prints the recall/precision number of bins to STDOUT."""
 
         print('\tRecall')
         print('Prec.', '\t'.join([str(r) for r in self.recalls]), sep='\t')
@@ -307,42 +366,4 @@ class BenchMarkResult:
             row = [self._binsfound[(recall, min_precision)] for recall in self.recalls]
             row.sort(reverse=True)
             print(min_precision, '\t'.join([str(i) for i in row]), sep='\t')        
-
-
-
-if __name__ == '__main__':
-
-    parserkws = {'prog': 'benchmark.py',
-                 'formatter_class': argparse.RawDescriptionHelpFormatter,
-                 'usage': '%(prog)s OBSERVED REFERENCE',
-                 'description': __doc__}
-
-    # Create the parser
-    parser = argparse.ArgumentParser(**parserkws)
-
-    parser.add_argument('observed', help='observed bins')
-    parser.add_argument('reference', help='true bins')
-
-    # Print help if no arguments are given
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit()
-
-    args = parser.parse_args()
-
-    if not os.path.isfile(args.observed):
-        raise FileNotFoundError(args.observed)
-
-    if not os.path.isfile(args.reference):
-        raise FileNotFoundError(args.reference)
-        
-    with open(args.reference) as filehandle:
-        reference = Reference.fromfile(filehandle)
-        
-    with open(args.observed) as filehandle:
-        observed = Observed.fromfile(filehandle, reference)
-        
-    results = BenchMarkResult(reference=reference, observed=observed)
-    
-    results.printmatrix()
 
