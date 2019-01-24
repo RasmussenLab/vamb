RasmussenLab/vamb

commit 9834a4caa042b3509f101e339835e3e01c8e5d59

General memory usage improvements
encode.py:
  make_dataloader now takes a `destroy` argument similar to cluster.cluster.
  This mutates input arrays in-place, and so does not create a copy, saving RAM.
  This is set to `False` by default, but set to `True` in run.py.

  `VAE.encode`'s output is now a native Numpy array instead of a converted Torch
  array. This means the Numpy object has ownership of the memory buffer, and can
  resize the array at will, allowing memory savings.

parsecontigs.py
  read_contigs now takes a `preallocate` argument, by default set to `True`.
  When `True`, reads through FASTA file twice, first to count number of seqs,
  and preallocate arrays, second to fill them. This doubles the time to parse
  the FASTA file, but significantly reduces the memory requirement.

parsebam.py
  Now flushes logfile after processing each BAM file.

cluster.py
  Contig labels are no longer masked, and thus no longer copied. Instead, masking
  operations are done on a smaller integer index array, saving memory while
  spending negligible extra time.

  `cluster._cluster` now automatically shuffles input arrays when instantiating
  the cluster iterator.

run.py
  As `cluster._cluster` now shuffles input, `run.py` no longer does.

Added a "Saving memory" section of doc/tutorial.html

Various other minor changes to docstrings, no functional impact.

cluster.py
@@ -24,6 +24,7 @@ cluster algorithm:
 
 import sys as _sys
 import os as _os
+import random as _random
 import numpy as _np
 from collections import defaultdict as _defaultdict
 import vamb.vambtools as _vambtools
@@ -115,6 +116,15 @@ def _cluster(matrix, labels, threshold, max_steps):
     # will never be present in any other cluster anyway.
     seed = 0
     keepmask = _np.ones(len(matrix), dtype=_np.bool)
+    indices = _np.arange(len(matrix), dtype=_np.uint32)
+
+    # Shuffle matrix and labels in unison. Not reproducible if using Numpy RNG
+    # when the two arrays have different dimensions.
+    rng = _random.Random(0)
+    state = rng.getstate()
+    rng.shuffle(matrix)
+    rng.setstate(state)
+    rng.shuffle(indices)
 
     while len(matrix) > 0:
         # Find medoid using iterative sampling function above
@@ -122,19 +132,23 @@ def _cluster(matrix, labels, threshold, max_steps):
         medoid, cluster = sampling
         seed += 1
 
-        # Write data to output
-        yield labels[medoid], set(labels[cluster])
+        # Write data to output. We use indices instead of labels directly to
+        # prevent masking and thereby duplicating large string arrays
+        if labels is None:
+            yield indices[medoid]+1, {i+1 for i in indices[cluster]}
+        else:
+            yield labels[indices[medoid]], {labels[indices[i]] for i in cluster}
 
         for point in cluster:
             keepmask[point] = False
 
         # Only remove cluster if we have more than 1 point in cluster
+        # because masking takes time
         if len(cluster) > 1 or seed == len(matrix):
             _vambtools.inplace_maskarray(matrix, keepmask)
-            labels = labels[keepmask]
-
-            # This is quicker than changing existing mask to True
-            keepmask = _np.ones(len(matrix), dtype=_np.bool)
+            indices = indices[keepmask] # no need to inplace mask
+            keepmask.resize(len(matrix), refcheck=False)
+            keepmask[:] = True
             seed = 0
 
 def _check_params(matrix, threshold, labels, nsamples, maxsize, maxsteps, logfile):
@@ -146,14 +160,12 @@ def _check_params(matrix, threshold, labels, nsamples, maxsize, maxsteps, logfil
     if len(matrix) < 1:
         raise ValueError('Matrix must have at least 1 observation.')
 
-    if labels is None:
-        labels = _np.arange(len(matrix)) + 1
+    if labels is not None:
+        if len(labels) != len(matrix):
+            raise ValueError('labels must have same length as matrix')
 
-    elif type(labels) != _np.ndarray or len(labels) != len(matrix):
-        raise ValueError('labels must be a 1D Numpy array with same length as matrix')
-
-    if len(set(labels)) != len(matrix):
-        raise ValueError('Labels must be unique')
+        if len(set(labels)) != len(matrix):
+            raise ValueError('Labels must be unique')
 
     if matrix.dtype != _np.float32:
         raise ValueError('Matrix must be of data type np.float32')
@@ -223,7 +235,7 @@ def write_clusters(filehandle, clusters, max_clusters=None, min_size=1,
 
     Inputs:
         filehandle: An open filehandle that can be written to
-        clusters: An iterator generated by function `clusters` or dict
+        clusters: An iterator generated by function `clusters` or a dict
         max_clusters: Stop printing after this many clusters [None]
         min_size: Don't output clusters smaller than N contigs
         header: Commented one-line header to add
@@ -236,7 +248,7 @@ def write_clusters(filehandle, clusters, max_clusters=None, min_size=1,
     if not hasattr(filehandle, 'writable') or not filehandle.writable():
         raise ValueError('Filehandle must be a writable file')
 
-    if iter(clusters) is not clusters:
+    if iter(clusters) is not clusters: # Is True if clusters is not iterator
         clusters = clusters.items()
 
     if header is not None and len(header) > 0:
@@ -252,9 +264,6 @@ def write_clusters(filehandle, clusters, max_clusters=None, min_size=1,
     ncontigs = 0
 
     for clustername, contigs in clusters:
-        if clusternumber == max_clusters:
-            break
-
         if len(contigs) < min_size:
             continue
 
@@ -266,9 +275,10 @@ def write_clusters(filehandle, clusters, max_clusters=None, min_size=1,
         clusternumber += 1
         ncontigs += len(contigs)
 
+        if clusternumber + 1 == max_clusters:
+            break
+
     return clusternumber, ncontigs
-
-
 
 def read_clusters(filehandle, min_size=1):
     """Read clusters from a file as created by function `writeclusters`.

doc/tutorial.html
@@ -11791,7 +11791,7 @@ div#notebook {
 <li><code>contigs.fna</code> - The filtered FASTA contigs which were mapped against, and</li>
 <li><code>bamfiles/*.bam</code> - The 6 .bam files from mapping the reads to the contigs above.</li>
 </ul>
-<h2 id="Table-of-contents:">Table of contents:<a class="anchor-link" href="#Table-of-contents:">&#182;</a></h2><h3 id="1.-Importing-Vamb-and-getting-help">1. <a href="#importing">Importing Vamb and getting help</a><a class="anchor-link" href="#1.-Importing-Vamb-and-getting-help">&#182;</a></h3><h3 id="2.-Calculate-the-sequence-tetranucleotide-frequencies">2. <a href="#parsecontigs">Calculate the sequence tetranucleotide frequencies</a><a class="anchor-link" href="#2.-Calculate-the-sequence-tetranucleotide-frequencies">&#182;</a></h3><h3 id="3.-Calculate-the-abundance-matrix">3. <a href="#parsebam">Calculate the abundance matrix</a><a class="anchor-link" href="#3.-Calculate-the-abundance-matrix">&#182;</a></h3><h3 id="4.-Train-the-autoencoder-and-encode-input-data">4. <a href="#encode">Train the autoencoder and encode input data</a><a class="anchor-link" href="#4.-Train-the-autoencoder-and-encode-input-data">&#182;</a></h3><h3 id="5.-Binning-the-encoding">5. <a href="#cluster">Binning the encoding</a><a class="anchor-link" href="#5.-Binning-the-encoding">&#182;</a></h3><h3 id="6.-Postprocessing-the-bins">6. <a href="#postprocessing">Postprocessing the bins</a><a class="anchor-link" href="#6.-Postprocessing-the-bins">&#182;</a></h3><h3 id="7.-Summary-of-full-workflow">7. <a href="#summary">Summary of full workflow</a><a class="anchor-link" href="#7.-Summary-of-full-workflow">&#182;</a></h3><h3 id="8.-Optional:-Benchmarking-your-bins">8. <a href="#benchmark">Optional: Benchmarking your bins</a><a class="anchor-link" href="#8.-Optional:-Benchmarking-your-bins">&#182;</a></h3><h3 id="9.-Potential-improvements-to-Vamb">9. <a href="#improvements">Potential improvements to Vamb</a><a class="anchor-link" href="#9.-Potential-improvements-to-Vamb">&#182;</a></h3><p><a id="importing"></a></p>
+<h2 id="Table-of-contents:">Table of contents:<a class="anchor-link" href="#Table-of-contents:">&#182;</a></h2><h3 id="1.-Importing-Vamb-and-getting-help">1. <a href="#importing">Importing Vamb and getting help</a><a class="anchor-link" href="#1.-Importing-Vamb-and-getting-help">&#182;</a></h3><h3 id="2.-Calculate-the-sequence-tetranucleotide-frequencies">2. <a href="#parsecontigs">Calculate the sequence tetranucleotide frequencies</a><a class="anchor-link" href="#2.-Calculate-the-sequence-tetranucleotide-frequencies">&#182;</a></h3><h3 id="3.-Calculate-the-abundance-matrix">3. <a href="#parsebam">Calculate the abundance matrix</a><a class="anchor-link" href="#3.-Calculate-the-abundance-matrix">&#182;</a></h3><h3 id="4.-Train-the-autoencoder-and-encode-input-data">4. <a href="#encode">Train the autoencoder and encode input data</a><a class="anchor-link" href="#4.-Train-the-autoencoder-and-encode-input-data">&#182;</a></h3><h3 id="5.-Binning-the-encoding">5. <a href="#cluster">Binning the encoding</a><a class="anchor-link" href="#5.-Binning-the-encoding">&#182;</a></h3><h3 id="6.-Postprocessing-the-bins">6. <a href="#postprocessing">Postprocessing the bins</a><a class="anchor-link" href="#6.-Postprocessing-the-bins">&#182;</a></h3><h3 id="7.-Summary-of-full-workflow">7. <a href="#summary">Summary of full workflow</a><a class="anchor-link" href="#7.-Summary-of-full-workflow">&#182;</a></h3><h3 id="8.-Running-VAMB-with-low-memory-(RAM)">8. <a href="#memory">Running VAMB with low memory (RAM)</a><a class="anchor-link" href="#8.-Running-VAMB-with-low-memory-(RAM)">&#182;</a></h3><h3 id="9.-Optional:-Benchmarking-your-bins">9. <a href="#benchmark">Optional: Benchmarking your bins</a><a class="anchor-link" href="#9.-Optional:-Benchmarking-your-bins">&#182;</a></h3><h3 id="10.-Potential-improvements-to-Vamb">10. <a href="#improvements">Potential improvements to Vamb</a><a class="anchor-link" href="#10.-Potential-improvements-to-Vamb">&#182;</a></h3><p><a id="importing"></a></p>
 <h2 id="Importing-Vamb-and-getting-help">Importing Vamb and getting help<a class="anchor-link" href="#Importing-Vamb-and-getting-help">&#182;</a></h2><p>First step is to get Vamb imported</p>
 
 <pre><code>[jakni@nissen:~]$ python
@@ -11990,16 +11990,16 @@ DESCRIPTION
     ...     tnfs, contignames, lengths = read_contigs(filehandle)
 
 FUNCTIONS
-    read_contigs(byte_iterator, minlength=100, dtype=&lt;class &#39;numpy.float32&#39;&gt;)
+    read_contigs(filehandle, minlength=100, preallocate=True, dtype=&lt;class &#39;numpy.float32&#39;&gt;)
         Parses a FASTA file open in binary reading mode.
         
         Input:
-            byte_iterator: Iterator of binary lines of a FASTA file
+            filehandle: Filehandle open in binary mode of a FASTA file
             minlength[100]: Ignore any references shorter than N bases
         
         Outputs:
             tnfs: An (n_FASTA_entries x 136) matrix of tetranucleotide freq.
-            contignames: A Numpy array of contig headers
+            contignames: A list of contig headers
             lengths: A Numpy array of contig lengths
 
 FILE
@@ -12129,6 +12129,7 @@ First 5 elements of lengths:
 <p>The <code>tnfs</code> is the tetranucleotide frequency - it's the frequency of the canonical kmer of each 4mer in the contig. We use 4-mers because there are 136 canonical 4-mers, which is an appropriate number of features to cluster - not so few that there's no signal and not so many it becomes unwieldy and the estimates of the frequencies become uncertain. We could also have used 3-mers. In tests we have made, 3-mers are <em>almost</em>, but not quite as good as 4-mers for separating different species. You could probably switch tetranucleotide frequency to trinucleotide frequency in Vamb without any significant drop of accuracy. However, there are 512 canonical 5-mers, that would be too many features to handle comfortably, and it could easily cause memory issues.</p>
 <p><strong>The argument <code>minlength</code></strong> sets the filter removing any contigs shorter than this. Short contigs in general do not work well with Vamb since neither TNF nor the abundance (shown in next section) can be estimated reliably for short contigs. When choosing the correct theshold, there is some sweet spot between on one hand not allowing small contigs to act as a source of noise in the binning, and on the other hand to not throw away more contigs than you absolutely have to. We don't know what the sweet spot is, but it's probably somewhere around ~2000 bp.</p>
 <p>The problem with filtering contigs using <code>minlength</code> is that the smaller contigs which are thrown away will still recruit reads during the mapping that creates the BAM files, thus removing information from those reads. For that reason, we recommend filtering the contigs <em>before</em> mapping by using the function <code>vamb.vambtools.filtercontigs</code>.</p>
+<p><strong>With the argument <code>preallocate</code></strong> set to true (as is default), the function will read the file <em>twice</em>, one time to count the number of sequences, then it will preallocate the arrays, and then it will read it a second time to fill the arrays. Having this set to <code>True</code> approximately doubles the reading time, but cuts down on memory.</p>
 <p><strong>The memory consumption of Vamb can be an issue</strong>, so at this point, you should probably consider whether you have enough RAM. If not, all the relevant modules have reading and writing functions so you can dump the results to disk and delete them from memory. This is a small dataset, so there's no problem. With hundreds of samples and millions of contigs however, this becomes a problem, even though Vamb is fairly memory-friendly.</p>
 <p>As a rule of thumb, the memory consumption for the most memory intensive step of Vamb is approximately 8 × (n_samples + 136) × n_contigs bytes plus a little bit of overhead. If this is significantly lower than your RAM, don't worry about it. If it's within a factor 2 of your available RAM, you'll need to delete objects you don't need anymore.</p>
 <p><a id="parsebam"></a></p>
@@ -13110,6 +13111,25 @@ Number of bins after filtering: 136
 </div>
 </div>
 
+</div>
+<div class="cell border-box-sizing text_cell rendered"><div class="prompt input_prompt">
+</div>
+<div class="inner_cell">
+<div class="text_cell_render border-box-sizing rendered_html">
+<p><a id="memory"></a></p>
+<h2 id="Running-VAMB-with-low-memory-(RAM)">Running VAMB with low memory (RAM)<a class="anchor-link" href="#Running-VAMB-with-low-memory-(RAM)">&#182;</a></h2><p>In the VAMB software, a series of tradeoffs can be taken to decrease RAM consumption, usually to the detriment of some other property like speed or convenience. With all of those tradeoffs taken, VAMB is relatively memory efficient. By default, several of these tradeoffs are are not taken when running from a Python interpreter, however they are all enabled when running VAMB with the <code>run.py</code> script.</p>
+<p>With all memory-saving options enabled, the memory consumption is on the order of $4*N_{contigs}*(136+N_{samples})$ bytes plus the memory required to keep all the sequence labels in memory, plus a little bit of overhead. As a rule of thumb, you should probably have $8*N_{contigs}*(160+N_{samples})$ bytes available.</p>
+<p>Here's a short list of all the available memory saving options:</p>
+<ul>
+<li>When using <code>vamb.parsecontigs.read_contigs</code>, set the <code>preallocate</code> keyword to <code>True</code>. This doubles the time spent in the function, but significantly reduces RAM usage.</li>
+<li>Pass a path to the <code>dumpdirectory</code> in the <code>vamb.parsecontigs.read_bamfiles</code> function. Temporary files will be written to files in this directory to avoid keeping them in memory. This takes a little bit of disk space, but saves lots of RAM)</li>
+<li>In the <code>encode.make_dataloader</code> function, set <code>destroy</code> to True. This will in-place modify your RPKM and TNF array, deleting unusable rows and normalizing them. This prevents the creation of a copy of the data.</li>
+<li>Similarly, set <code>destroy=True</code> when using the <code>cluster.cluster</code> function. Again, the input (latent) datasets are modified in-place, saving a copy of the data. This function will completely consume the input array.</li>
+<li>When clustering, instead of generating clusters in memory, instantiate the cluster iterator and pass it directly to <code>cluster.write_clusters</code>. This way, each cluster will be written to disk after creation and not accumulate in memory. This obviously requires disk space, and you will have to reload the clusters if you are planning on using them.</li>
+</ul>
+
+</div>
+</div>
 </div>
 <div class="cell border-box-sizing text_cell rendered"><div class="prompt input_prompt">
 </div>

encode.py
@@ -31,11 +31,11 @@ import vamb.vambtools as _vambtools
 if _torch.__version__ < '0.4':
     raise ImportError('PyTorch version must be 0.4 or newer')
 
-def make_dataloader(rpkm, tnf, batchsize=64, cuda=False):
+def make_dataloader(rpkm, tnf, batchsize=64, destroy=False, cuda=False):
     """Create a DataLoader and a contig mask from RPKM and TNF.
 
     The dataloader is an object feeding minibatches of contigs to the VAE.
-    The data are normalized versions of the input datasets, with "zero" contigs,
+    The data are normalized versions of the input datasets, with zero-contigs,
     i.e. contigs where a row in either TNF or RPKM are all zeros, removed.
     The mask is a boolean mask designating which contigs have been kept.
 
@@ -43,7 +43,8 @@ def make_dataloader(rpkm, tnf, batchsize=64, cuda=False):
         rpkm: RPKM matrix (N_contigs x N_samples)
         tnf: TNF matrix (N_contigs x 136)
         batchsize: Starting size of minibatches for dataloader
-        cuda: Pagelock memory of dataloader (use when using CUDA)
+        destroy: Mutate rpkm and tnf array in-place instead of making a copy.
+        cuda: Pagelock memory of dataloader (use when using GPU acceleration)
 
     Outputs:
         DataLoader: An object feeding data to the VAE
@@ -62,23 +63,26 @@ def make_dataloader(rpkm, tnf, batchsize=64, cuda=False):
     if tnf.shape[1] != 136:
         raise ValueError('TNF must be 136 long along axis 1')
 
-    depthssum = rpkm.sum(axis=1)
     tnfsum = tnf.sum(axis=1)
-    mask = depthssum != 0
-    mask &= tnfsum != 0
+    mask = tnfsum != 0
+    del tnfsum
+    depthssum = rpkm.sum(axis=1)
+    mask &= depthssum != 0
 
-    # Remove zero-observations Unfortunately, a copy is necessary.
-    # It doesn't matter much, as cluster.py will create a copy anyway when.
-    # removing observations during clustering. This cannot easily prevented.
-    rpkm_copy = rpkm[mask].astype(_np.float32)
-    tnf_copy = tnf[mask].astype(_np.float32)
+    if destroy:
+        rpkm = _vambtools.inplace_maskarray(rpkm, mask)
+        tnf = _vambtools.inplace_maskarray(tnf, mask)
+    else:
+        rpkm = rpkm[mask].astype(_np.float32)
+        tnf = tnf[mask].astype(_np.float32)
+
     depthssum = depthssum[mask]
 
     # Normalize arrays and create the Tensors
-    rpkm_copy /= depthssum.reshape((-1, 1))
-    _vambtools.zscore(tnf_copy, axis=0, inplace=True)
-    depthstensor = _torch.from_numpy(rpkm_copy)
-    tnftensor = _torch.from_numpy(tnf_copy)
+    rpkm /= depthssum.reshape((-1, 1))
+    _vambtools.zscore(tnf, axis=0, inplace=True)
+    depthstensor = _torch.from_numpy(rpkm)
+    tnftensor = _torch.from_numpy(tnf)
 
     # Create dataloader
     dataset = _TensorDataset(depthstensor, tnftensor)
@@ -298,8 +302,10 @@ class VAE(_nn.Module):
         depths_array, tnf_array = data_loader.dataset.tensors
         length = len(depths_array)
 
-        latent = _torch.zeros((length, self.nlatent), dtype=data_loader.dataset.tensors[0].dtype)
-        assert not latent.is_cuda # Should be on CPU, not GPU
+        # We make a Numpy array instead of a Torch array because, if we create
+        # a Torch array, then convert it to Numpy, Numpy will believe it doesn't
+        # own the memory block, and array resizes will not be permitted.
+        latent = _np.empty((length, self.nlatent), dtype=_np.float32)
 
         row = 0
         with _torch.no_grad():
@@ -319,9 +325,7 @@ class VAE(_nn.Module):
                 row += len(mu)
 
         assert row == length
-        latent_array = latent.detach().numpy()
-
-        return latent_array
+        return latent
 
     def save(self, filehandle):
         """Saves the VAE to a path or binary opened file. Load with VAE.load

parsebam.py
@@ -221,6 +221,7 @@ def read_bamfiles(paths, dumpdirectory=None, minscore=50, minlength=100,
         def _callback(result):
             path, rpkms, length = result
             print('\tProcessed ', path, file=logfile)
+            logfile.flush()
 
         def _error_callback(result):
             print('\tERROR WHEN PROCESSING ', path, file=logfile)

parsecontigs.py
@@ -12,40 +12,70 @@ import numpy as _np
 import gzip as _gzip
 import vamb.vambtools as _vambtools
 
-def read_contigs(byte_iterator, minlength=100, dtype=_np.float32):
-    """Parses a FASTA file open in binary reading mode.
-
-    Input:
-        byte_iterator: Iterator of binary lines of a FASTA file
-        minlength[100]: Ignore any references shorter than N bases
-
-    Outputs:
-        tnfs: An (n_FASTA_entries x 136) matrix of tetranucleotide freq.
-        contignames: A Numpy array of contig headers
-        lengths: A Numpy array of contig lengths
-    """
-
-    tnf_list = list()
+def _read_contigs_online(filehandle, minlength, dtype):
+    tnfs = list()
     contignames = list()
     lengths = list()
 
-    if byte_iterator is not iter(byte_iterator):
-        raise ValueError('byte_iterator is not an iterator')
-
-    entries = _vambtools.byte_iterfasta(byte_iterator)
+    entries = _vambtools.byte_iterfasta(filehandle)
 
     for entry in entries:
         if len(entry) < minlength:
             continue
 
-        tnf_list.append(entry.fourmer_freq())
+        tnfs.append(entry.fourmer_freq())
         contignames.append(entry.header)
         lengths.append(len(entry))
 
-    tnfs = _np.array(tnf_list, dtype=dtype)
-    del tnf_list
-
     lengths = _np.array(lengths, dtype=_np.int)
-    contignames = _np.array(contignames)
+    tnfs = _np.array(tnfs, dtype=dtype)
 
     return tnfs, contignames, lengths
+
+def _read_contigs_preallocated(filehandle, minlength, dtype):
+    n_entries = 0
+    entries = _vambtools.byte_iterfasta(filehandle)
+
+    for entry in entries:
+         if len(entry) >= minlength:
+             n_entries += 1
+
+    tnfs = _np.empty((n_entries, 136), dtype=dtype)
+    contignames = [None] * n_entries
+    lengths = _np.empty(n_entries, dtype=_np.int)
+
+    filehandle.seek(0) # Go back to beginning of file
+    entries = _vambtools.byte_iterfasta(filehandle)
+    entrynumber = 0
+    for entry in entries:
+        if len(entry) < minlength:
+            continue
+
+        tnfs[entrynumber] = entry.fourmer_freq()
+        contignames[entrynumber] = entry.header
+        lengths[entrynumber] = len(entry)
+
+        entrynumber += 1
+
+    return tnfs, contignames, lengths
+
+def read_contigs(filehandle, minlength=100, preallocate=True, dtype=_np.float32):
+    """Parses a FASTA file open in binary reading mode.
+
+    Input:
+        filehandle: Filehandle open in binary mode of a FASTA file
+        minlength[100]: Ignore any references shorter than N bases
+
+    Outputs:
+        tnfs: An (n_FASTA_entries x 136) matrix of tetranucleotide freq.
+        contignames: A list of contig headers
+        lengths: A Numpy array of contig lengths
+    """
+
+    if minlength < 4:
+        raise ValueError('Minlength must be at least 4')
+
+    if preallocate:
+        return _read_contigs_preallocated(filehandle, minlength, dtype)
+    else:
+        return _read_contigs_online(filehandle, minlength, dtype)

run.py
@@ -23,7 +23,9 @@ def calc_tnf(outdir, fastapath, mincontiglength, logfile):
     log('\nCalculating TNF', logfile, 0)
 
     with open(fastapath, 'rb') as tnffile:
-        ret = vamb.parsecontigs.read_contigs(tnffile, minlength=mincontiglength)
+        ret = vamb.parsecontigs.read_contigs(tnffile,
+                                             minlength=mincontiglength,
+                                             preallocate=True)
     tnfs, contignames, contiglengths = ret
 
     vamb.vambtools.write_npz(os.path.join(outdir, 'tnf.npz'), tnfs)
@@ -76,19 +78,21 @@ def trainvae(outdir, rpkms, tnfs, nhiddens, nlatent, alpha, beta, dropout, cuda,
                             alpha=alpha, beta=beta, dropout=dropout, cuda=cuda)
 
     log('Created VAE', logfile, 1)
-    dataloader, mask = vamb.encode.make_dataloader(rpkms, tnfs, batchsize, cuda)
+    dataloader, mask = vamb.encode.make_dataloader(rpkms, tnfs, batchsize,
+                                                   destroy=True, cuda=cuda)
     log('Created dataloader and mask', logfile, 1)
     n_discarded = len(mask) - mask.sum()
-    log('Number of contigs unsuitable for encoding: {}'.format(n_discarded), logfile, 2)
-    log('Number of contigs remaining: {}'.format(len(mask) - n_discarded), logfile, 2)
+    log('Number of contigs unsuitable for encoding: {}'.format(n_discarded), logfile, 1)
+    log('Number of contigs remaining: {}'.format(len(mask) - n_discarded), logfile, 1)
 
     modelpath = os.path.join(outdir, 'model.pt')
-    log('Training VAE\n', logfile, 1)
+    log('\nTraining VAE\n', logfile, 1)
     vae.trainmodel(dataloader, nepochs=nepochs, lrate=lrate, batchsteps=batchsteps,
                   logfile=logfile, modelfile=modelpath)
 
     latent = vae.encode(dataloader)
     vamb.vambtools.write_npz(os.path.join(outdir, 'latent.npz'), latent)
+    del vae # Needed to free "latent" array's memory references?
 
     elapsed = round(time.time() - begintime, 2)
     print('', file=logfile)
@@ -100,17 +104,6 @@ def cluster(outdir, latent, contignames, maxclusters, minclustersize, logfile):
     begintime = time.time()
 
     log('\nClustering', logfile)
-    # We shuffle to prevent bias in which contigs are picked as seeds
-    log('Shuffling latent encoding and contignames.', logfile, 1)
-    rng = np.random.RandomState(9082374)
-
-    state = rng.get_state()
-    rng.shuffle(latent)
-
-    rng.set_state(state)
-    rng.shuffle(contignames)
-
-    log('Clustering contigs', logfile, 1)
     clusteriterator = vamb.cluster.cluster(latent, labels=contignames,
                                            logfile=logfile, destroy=True)
 
@@ -147,7 +140,7 @@ def main(outdir, fastapath, bampaths, mincontiglength, minalignscore, subprocess
                            dropout, cuda, batchsize, nepochs, lrate, batchsteps, logfile)
 
     del tnfs, rpkms
-    contignames = contignames[mask]
+    contignames = [c for c, m in zip(contignames, mask) if m]
 
     # Cluster, save tsv file
     cluster(outdir, latent, contignames, maxclusters, minclustersize, logfile)

commit f7f9ceb788086a6a80a7ffe0c6209658126902a9

Fixed poor clustering performance
This commit fixes a bug introduced in commit 9834a4caa042b3509f101e339835e3e01c8e5d59 the 26th of November 2018. In that commit, memory consumption of clustering is reduced by shuffling the input matrix
in-place in the `vamb.cluster._cluster` function. However, inplace-shuffling of two-dimensional Numpy array using `random.shuffle` does not work properly, resulting in corruption of the latent encoding,
leading to poor clustering.

cluster.py
@@ -116,15 +116,11 @@ def _cluster(matrix, labels, threshold, max_steps):
     # will never be present in any other cluster anyway.
     seed = 0
     keepmask = _np.ones(len(matrix), dtype=_np.bool)
-    indices = _np.arange(len(matrix), dtype=_np.uint32)
+    indices = _np.random.RandomState(0).permutation(len(matrix))
 
-    # Shuffle matrix and labels in unison. Not reproducible if using Numpy RNG
-    # when the two arrays have different dimensions.
-    rng = _random.Random(0)
-    state = rng.getstate()
-    rng.shuffle(matrix)
-    rng.setstate(state)
-    rng.shuffle(indices)
+    # Shuffle matrix in order to prevent seed bias. `indices` keep track of
+    # which row is which.
+    matrix = matrix[indices]
 
     while len(matrix) > 0:
         # Find medoid using iterative sampling function above