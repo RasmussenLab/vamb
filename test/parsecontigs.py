Fix bugs when masking Composition
* Also set metadata minlength when length filtering Composition
* Fix the filter mask computed during length filtering of CompositionMetaData

test/parsecontigs.py
@@ -8,6 +8,7 @@ import vamb
 import testtools
 from vamb.parsecontigs import Composition, CompositionMetaData
 
+
 class TestReadContigs(unittest.TestCase):
     records = []
     io = io.BytesIO()
@@ -39,26 +40,34 @@ class TestReadContigs(unittest.TestCase):
         composition = Composition.from_file(self.io, minlength=450)
         md = composition.metadata
         hash1 = md.refhash
+
         composition.filter_min_length(minlen)
+        n_initial_seq = md.nseqs
 
         hash2 = md.refhash
         self.assertNotEqual(hash1, hash2)
         self.assertEqual(len(md.identifiers), len(md.lengths))
         self.assertEqual(md.nseqs, md.mask.sum())
-        self.assertLessEqual(minlen, composition.metadata.lengths.min(initial=minlen))
+        self.assertLessEqual(
+            minlen, composition.metadata.lengths.min(initial=minlen))
         self.assertEqual(len(md.mask), len(self.records))
 
+        # NB: Here we filter metadata without filtering the composition.
+        # That means from this point on, the metadata and comp is out of sync,
+        # and comp is invalid.
         md.filter_min_length(minlen + 50)
         self.assertEqual(len(md.identifiers), len(md.lengths))
         self.assertEqual(md.nseqs, md.mask.sum())
-        self.assertLessEqual(minlen, composition.metadata.lengths.min(initial=minlen + 50))
+        self.assertLessEqual(
+            minlen, composition.metadata.lengths.min(initial=minlen + 50))
         self.assertEqual(len(md.mask), len(self.records))
+        self.assertLess(md.nseqs, n_initial_seq)
 
         hash3 = md.refhash
         md.filter_min_length(minlen - 50)
         self.assertEqual(hash3, md.refhash)
 
-        composition.filter_min_length(50000000000)
+        md.filter_min_length(50000000000)
         self.assertEqual(md.nseqs, 0)
         self.assertFalse(np.any(md.mask))
 
@@ -70,18 +79,22 @@ class TestReadContigs(unittest.TestCase):
         composition = Composition.from_file(self.io, minlength=420)
         passed = list(filter(lambda x: len(x.sequence) >= 420, self.records))
 
-        self.assertEqual(composition.nseqs, len(composition.metadata.identifiers))
+        self.assertEqual(composition.nseqs, len(
+            composition.metadata.identifiers))
         self.assertEqual(composition.nseqs, len(composition.metadata.lengths))
 
         self.assertTrue(composition.matrix.dtype, np.float32)
         self.assertEqual(composition.matrix.shape, (len(passed), 103))
 
         # Names
-        self.assertEqual(list(composition.metadata.identifiers), [i.header for i in passed])
+        self.assertEqual(list(composition.metadata.identifiers), [
+                         i.header for i in passed])
 
         # Lengths
-        self.assertTrue(np.issubdtype(composition.metadata.lengths.dtype, np.integer))
-        self.assertEqual([len(i.sequence) for i in passed], list(composition.metadata.lengths))
+        self.assertTrue(np.issubdtype(
+            composition.metadata.lengths.dtype, np.integer))
+        self.assertEqual([len(i.sequence) for i in passed],
+                         list(composition.metadata.lengths))
 
     def test_save_load(self):
         buf = io.BytesIO()
@@ -91,7 +104,7 @@ class TestReadContigs(unittest.TestCase):
         buf.seek(0)
         composition_2 = Composition.load(buf)
         md2 = composition_2.metadata
-        
+
         self.assertTrue(np.all(composition_1.matrix == composition_2.matrix))
         self.assertTrue(np.all(md1.identifiers == md2.identifiers))
         self.assertTrue(np.all(md1.lengths == md2.lengths))