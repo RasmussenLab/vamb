e4c58bffddaa887aa211f5b7d6650f934ee1a36e
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2022-11-24 10:16:51 +0100

    Bump PyTorch to 1.13

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ SETUP_METADATA = \
     "url": "https://github.com/RasmussenLab/vamb",
     "author": "Jakob Nybo Nissen and Simon Rasmussen",
     "author_email": "jakobnybonissen@gmail.com",
-    "version": "3.0.8",
+    "version": "3.0.9",
     "license": "MIT",
     "packages": find_packages(),
     "package_data": {"vamb": ["kernel.npz"]},
@@ -22,7 +22,7 @@ SETUP_METADATA = \
     "ext_modules": [Extension("vamb._vambtools",
                                sources=["src/_vambtools.pyx"],
                                language="c")],
-    "install_requires": ["numpy>=1.20", "torch>=1.8", "pysam>=0.14"],
+    "install_requires": ["numpy>=1.20", "torch>=1.13", "pysam>=0.14"],
     "setup_requires": ['Cython>=0.29', "setuptools>=58.0"],
     "python_requires": ">=3.5",
     "classifiers":[
