34a818e5891edb3ca3815dd5e3de3302008c7f86
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-02-01 18:29:46 +0100

    Script create_cluster_scores_bin_path: Add type hints

--- src/create_cluster_scores_bin_path_dict.py
+++ src/create_cluster_scores_bin_path_dict.py
@@ -3,14 +3,17 @@ import os
 import json
 import argparse
 
+from typing import cast
 
-def get_cluster_score_bin_path(path_checkm_all, path_bins, bins_set):
 
+def get_cluster_score_bin_path(
+    path_checkm_all: str, path_bins: str, bins: set[str]
+) -> tuple[dict[str, tuple[float, float]], dict[str, str]]:
     """Given CheckM has been run for all samples, create 2 dictionaries:
     - {bin:path_bin}
     - {bin:[completeness, contamination]}"""
-    cluster_score = dict()
-    bin_path = dict()
+    cluster_score: dict[str, tuple[float, float]] = dict()
+    bin_path: dict[str, str] = dict()
     for sample in os.listdir(path_checkm_all):
         path_quality_s = os.path.join(path_checkm_all, sample, "quality_report.tsv")
         c_com_con = np.loadtxt(
@@ -24,10 +27,11 @@ def get_cluster_score_bin_path(path_checkm_all, path_bins, bins_set):
 
         for row in c_com_con:
             cluster, com, con = row
+            cluster = cast(str, cluster)
             com, con = float(com), float(con)
             bin_name = cluster + ".fna"
-            if bin_name in bins_set:
-                cluster_score[cluster] = [com, con]
+            if bin_name in bins:
+                cluster_score[cluster] = (com, con)
                 bin_path[cluster + ".fna"] = os.path.join(
                     path_bins, sample, cluster + ".fna"
                 )
