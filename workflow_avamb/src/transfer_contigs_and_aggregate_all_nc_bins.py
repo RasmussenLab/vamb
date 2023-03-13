789b7a90acc62c6357843dad814b92509d02ffcb
Paupiera <38884866+Paupiera@users.noreply.github.com>
2023-03-13 10:14:18 +0100

    Update workflow_avamb/src/transfer_contigs_and_aggregate_all_nc_bins.py
    Co-authored-by: Jakob Nybo Nissen <jakobnybonissen@gmail.com>

--- workflow_avamb/src/transfer_contigs_and_aggregate_all_nc_bins.py
+++ workflow_avamb/src/transfer_contigs_and_aggregate_all_nc_bins.py
@@ -172,9 +172,6 @@ def mv_single_ripped_nc_bins(
                     "Bin %s was ripped because of meaningless edges or pairing and afterwards no intersection was shared with any other bin so it is moved from %s to %s"
                     % (cluster, src_bin, trg_bin)
                 )
-                
-                #shutil.move(src_bin, trg_bin)
-                #shutil.copy(src_bin, trg_bin)
                 os.symlink(src_bin, trg_bin)
 
 
