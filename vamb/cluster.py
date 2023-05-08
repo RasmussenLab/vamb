RasmussenLab/vamb

commit fc31a8ac1d9aaaeafee0dae820d007c1d744624b

Refine zeromask check in latent normalization
Previously, to check if every feature of a contig's latent representation was
zero, Vamb checked if the mean was zero. This caused a small risk that a contig
with nonzero latent space would happen to sum to zero.
Now, instead check if the largest value is zero.

vamb/cluster.py
@@ -479,7 +479,8 @@ def _normalize(matrix: _Tensor, inplace: bool = False) -> _Tensor:
 
     # If any rows are kept all zeros, the distance function will return 0.5 to all points
     # inclusive itself, which can break the code in this module
-    zeromask = matrix.sum(dim=1) == 0
+    matrix[matrix < 0] = 0
+    zeromask = matrix.max(dim=1).values == 0
     matrix[zeromask] = 1 / matrix.shape[1]
     matrix /= matrix.norm(dim=1).reshape(-1, 1) * (2**0.5)
     return matrix

commit e4059d72c1b3d44ce2b74e29a1a61cc18dd1b91d

Bugfix: Do not RELU latent in clustering
Commit fc31a8a accidentally set `matrix[matrix < 0] = 0`, hence destroying 50%
of information in the latent space. This led to a serious quality regression
in Vamb, as discovered by Xinyuan.

Also check that the new PCG64-based shuffling works correctly.

vamb/cluster.py
@@ -479,8 +479,7 @@ def _normalize(matrix: _Tensor, inplace: bool = False) -> _Tensor:
 
     # If any rows are kept all zeros, the distance function will return 0.5 to all points
     # inclusive itself, which can break the code in this module
-    matrix[matrix < 0] = 0
-    zeromask = matrix.max(dim=1).values == 0
+    zeromask = (matrix == 0).all(dim=1)
     matrix[zeromask] = 1 / matrix.shape[1]
     matrix /= matrix.norm(dim=1).reshape(-1, 1) * (2**0.5)
     return matrix

test/test_cluster.py
@@ -35,9 +35,10 @@ class TestClusterer(unittest.TestCase):
     # This depends on the implementation of shuffling and permute being
     # the same, which I test here.
     def test_shuffling(self):
+        seed = 0
         cp = self.data.copy()
-        np.random.RandomState(0).shuffle(cp)
-        indices = np.random.RandomState(0).permutation(len(cp))
+        np.random.Generator(np.random.PCG64(seed)).shuffle((cp))
+        indices = np.random.Generator(np.random.PCG64(seed)).permutation(len(cp))
         cplike = self.data[indices]
         self.assertTrue(np.all(cplike == cp))
 