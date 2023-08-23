56c1ec913d953a6e68542955bd1c579678692305
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-08-23 10:35:01 +0200

    Add marker gene detection
    Add the ability of Vamb to detect marker genes. This is done using pyrodigal
    to predict genes in the contigs, and pyhmmer to find marker genes in the
    predicted genes.
    
    The marker genes are currently not used in Vamb, so future commits need to add
    the ability of Vamb to enhance binning based on these marker genes.

--- vamb/__init__.py
+++ vamb/__init__.py
@@ -7,6 +7,7 @@ __version__ = (4, 1, 3)
 from . import vambtools
 from . import parsebam
 from . import parsecontigs
+from . import parsemarkers
 from . import cluster
 from . import encode
 from . import aamb_encode
@@ -15,6 +16,7 @@ __all__ = [
     "vambtools",
     "parsebam",
     "parsecontigs",
+    "parsemarkers",
     "cluster",
     "encode",
     "aamb_encode",
