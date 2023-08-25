655400932531aae490294ca7c8a9d2dfb9f12b27
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-08-25 10:03:40 +0200

    Rehaul and tweak clustering
    This commit is a squashed version of several commits that can be found on the
    exp_cluster branch, and which has been tested individually. It contains the
    following changes:
    
    Seed ordering
    
    Before, the order in which seed contigs were picked was randomized, in order to
    prevent bias arising from the order of the sequences in the FASTA files.
    Now, we pick the seeds in a precomputed order, here the contig length.
    The idea is that the contigs which we are most confident should belong to a good
    cluster gets "first pick" during clustering to see if they can form proper
    clusters.
    
    Length weighting
    
    When computing the distance histogram from which the threshold is found, contigs
    are now weighed by their length. This should make it more likely to correctly
    detect clusters composed of a small number of large contigs.
    In testing, this improves binning result, and the distance histograms become
    clearer and more well-shaped.
    
    Simplified medoid search
    
    Before, the medoid should have the lowest mean distance to each contig within
    a small radius. Now, since we take length into accounts when finding the
    threshold, instead choose the medoid that has the highest "local density".
    This is computed as sum(length * (R - distance)) where R is a small contant,
    for each contig where distance <= R.
    Also, the random sampling of medoids is now simpler and should be more efficient
    
    Various refactorings and tweaks
    
    * Update the hyperparameters windowsize and minsuccesses to make Vamb the
      cluster selection criteria stricter
    * Implement a pack function which deleted used points in the matrix and
      associated vectors. This enables better control of when the matrix is packed,
      which should make clustering faster
    * When the peak valley ratio is raised, start over from the highest ordered
      seed. This makes sure the highest ordered seeds are attempted multiple times.
    * Misc refactoring

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -9,25 +9,30 @@ import vamb
 
 class TestClusterer(unittest.TestCase):
     data = np.random.random((1024, 40)).astype(np.float32)
+    lens = np.random.randint(500, 1000, size=1024)
 
     def test_bad_params(self):
         with self.assertRaises(ValueError):
-            vamb.cluster.ClusterGenerator(self.data.astype(np.float64))
+            vamb.cluster.ClusterGenerator(self.data.astype(np.float64), self.lens)
 
         with self.assertRaises(ValueError):
-            vamb.cluster.ClusterGenerator(self.data, maxsteps=0)
+            vamb.cluster.ClusterGenerator(self.data, self.lens, maxsteps=0)
 
         with self.assertRaises(ValueError):
-            vamb.cluster.ClusterGenerator(self.data, windowsize=0)
+            vamb.cluster.ClusterGenerator(self.data, self.lens, windowsize=0)
 
         with self.assertRaises(ValueError):
-            vamb.cluster.ClusterGenerator(self.data, minsuccesses=0)
+            vamb.cluster.ClusterGenerator(self.data, self.lens, minsuccesses=0)
 
         with self.assertRaises(ValueError):
-            vamb.cluster.ClusterGenerator(self.data, minsuccesses=5, windowsize=4)
+            vamb.cluster.ClusterGenerator(
+                self.data, self.lens, minsuccesses=5, windowsize=4
+            )
 
         with self.assertRaises(ValueError):
-            vamb.cluster.ClusterGenerator(np.random.random((0, 40)))
+            vamb.cluster.ClusterGenerator(
+                np.random.random((0, 40)), np.array([], dtype=int)
+            )
 
     # In the code, in __init__ of the cluster generator, the input matrix
     # is shuffled, and an index array is permuted to keep track of which
@@ -43,7 +48,7 @@ class TestClusterer(unittest.TestCase):
         self.assertTrue(np.all(cplike == cp))
 
     def test_basics(self):
-        clstr = vamb.cluster.ClusterGenerator(self.data)
+        clstr = vamb.cluster.ClusterGenerator(self.data, self.lens)
         self.assertIs(clstr, iter(clstr))
 
         x = next(clstr)
@@ -63,9 +68,9 @@ class TestClusterer(unittest.TestCase):
 
     def test_detruction(self):
         copy = self.data.copy()
-        clstr = vamb.cluster.ClusterGenerator(self.data)
+        clstr = vamb.cluster.ClusterGenerator(self.data, self.lens)
         self.assertTrue(np.any(np.abs(self.data - clstr.matrix.numpy()) > 0.001))
-        clstr = vamb.cluster.ClusterGenerator(copy, destroy=True)
+        clstr = vamb.cluster.ClusterGenerator(copy, self.lens, destroy=True)
         self.assertTrue(np.all(np.abs(copy - clstr.matrix.numpy()) < 1e-6))
         self.assertTrue(np.any(np.abs(self.data - clstr.matrix.numpy()) > 0.001))
 
@@ -80,21 +85,21 @@ class TestClusterer(unittest.TestCase):
 
     def test_normalization(self):
         hash_before = md5(self.data.data.tobytes()).digest().hex()
-        vamb.cluster.ClusterGenerator(self.data)
+        vamb.cluster.ClusterGenerator(self.data, self.lens)
         self.assertEqual(hash_before, md5(self.data.data.tobytes()).digest().hex())
         cp = self.data.copy()
-        vamb.cluster.ClusterGenerator(cp, destroy=True)
+        vamb.cluster.ClusterGenerator(cp, self.lens, destroy=True)
         hash_after = md5(cp.data.tobytes()).digest().hex()
         self.assertNotEqual(hash_before, hash_after)
 
         # Rows are permuted by the clusterer. We use xor to check the rows
         # are still essentially the same.
         before_xor = self.xor_rows_hash(cp)
-        vamb.cluster.ClusterGenerator(cp, destroy=True, normalized=True)
+        vamb.cluster.ClusterGenerator(cp, self.lens, destroy=True, normalized=True)
         self.assertEqual(before_xor, self.xor_rows_hash(cp))
 
     def test_cluster(self):
-        x = next(vamb.cluster.ClusterGenerator(self.data))
+        x = next(vamb.cluster.ClusterGenerator(self.data, self.lens))
         self.assertIsInstance(x.members, np.ndarray)
         med, st = x.as_tuple()
         self.assertIsInstance(st, set)
@@ -105,19 +110,20 @@ class TestClusterer(unittest.TestCase):
 class TestPairs(unittest.TestCase):
     n_samples = 1024
     data = np.random.random((n_samples, 40)).astype(np.float32)
+    lens = np.random.randint(500, 1000, size=1024)
 
     @staticmethod
     def randstring(len):
         return "".join(random.choices(string.ascii_letters, k=len))
 
     def test_too_few_names(self):
-        clstr = vamb.cluster.ClusterGenerator(self.data)
+        clstr = vamb.cluster.ClusterGenerator(self.data, self.lens)
         nameset = {self.randstring(10) for i in range(len(self.data) - 1)}
         with self.assertRaises(ValueError):
             list(vamb.cluster.pairs(clstr, list(nameset)))
 
     def test_pairs(self):
-        clstr = vamb.cluster.ClusterGenerator(self.data)
+        clstr = vamb.cluster.ClusterGenerator(self.data, self.lens)
         nameset = {self.randstring(10) for i in range(len(self.data))}
         pairs = list(vamb.cluster.pairs(clstr, list(nameset)))
 
