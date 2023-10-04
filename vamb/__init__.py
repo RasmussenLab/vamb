35b71823946352b127506d5e11b10d419a85a298
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-10-04 09:29:13 +0200

    Remove obsolete benchmark module and unused files
    The benchmark module has been superseded by our new benchmarking package at
     https://github.com/jakobnissen/VambBenchmarks.jl.

--- vamb/__init__.py
+++ vamb/__init__.py
@@ -1,22 +1,5 @@
 """Vamb - Variational Autoencoders for Metagenomic Binning
 Documentation: https://github.com/RasmussenLab/vamb/
-
-Vamb contains the following modules:
-vamb.vambtools
-vamb.parsecontigs
-vamb.parsebam
-vamb.encode
-vamb.cluster
-vamb.benchmark
-
-General workflow:
-1) Filter contigs by size using vamb.vambtools.filtercontigs
-2) Map reads to contigs to obtain BAM files
-3) Calculate a Composition of contigs using vamb.parsecontigs
-4) Create Abundance object from BAM files using vamb.parsebam
-5) Train autoencoder using vamb.encode
-6) Cluster latent representation using vamb.cluster
-7) Split bins using vamb.vambtools
 """
 
 __version__ = (4, 1, 3)
@@ -25,7 +8,6 @@ from . import vambtools
 from . import parsebam
 from . import parsecontigs
 from . import cluster
-from . import benchmark
 from . import encode
 from . import aamb_encode
 
@@ -34,7 +16,6 @@ __all__ = [
     "parsebam",
     "parsecontigs",
     "cluster",
-    "benchmark",
     "encode",
     "aamb_encode",
 ]
