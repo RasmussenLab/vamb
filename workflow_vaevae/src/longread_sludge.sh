b6cb407828b5c17b29a67c18a94a94ed7b82d5f4
Svetlana Kutuzova <galkina.s.a@gmail.com>
2023-10-24 15:14:48 +0200

    chore: runs

--- workflow_vaevae/src/longread_sludge.sh
+++ workflow_vaevae/src/longread_sludge.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/bash
-
+annotator=$1
+thres=$2
     # --taxonomy_predictions /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevaeout_sp100/results_taxonomy_predictor.csv \
 
 
-vamb \
-    --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevaeout_dadam \
-    --fasta /home/projects/cpr_10006/projects/semi_vamb/data/sludge/contigs_2kbp.fna \
-    --rpkm /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout/abundance.npz \
-    --taxonomy /home/projects/cpr_10006/people/paupie/vaevae/mmseq2_annotations/long_read_sludge/lr_sludge_taxonomy.tsv \
-    -l 64 \
-    -e 500 \
-    -q  \
-    -pe 100 \
-    -pq  \
-    --n_species 100 \
-    -o C \
-    --cuda \
-    --minfasta 200000
+# vamb \
+#     --model vaevae \
+#     --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevae_${annotator}_predictor_${thres} \
+#     --fasta /home/projects/cpr_10006/projects/semi_vamb/data/sludge/contigs_2kbp.fna \
+#     --rpkm /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout/abundance.npz \
+#     --taxonomy /home/projects/cpr_10006/people/svekut/04_mmseq2/taxonomy_cami_kfold/sludge_taxonomy_${annotator}.tsv \
+#     -l 64 \
+#     -e 1000 \
+#     -t 1024 \
+#     -q  \
+#     -pe 100 \
+#     -pt 1024 \
+#     -pq  \
+#     -pthr ${thres} \
+#     -o C \
+#     --cuda \
+#     --minfasta 200000
 
 vamb \
     --model reclustering \
-    --latent_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevaeout_dadam/vaevae_latent.npy \
-    --clusters_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevaeout_dadam/vaevae_clusters.tsv  \
+    --latent_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevae_${annotator}_predictor_${thres}/vaevae_latent.npy \
+    --clusters_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevae_${annotator}_predictor_${thres}/vaevae_clusters.tsv  \
     --fasta /home/projects/cpr_10006/projects/semi_vamb/data/sludge/contigs_2kbp.fna \
     --rpkm /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout/abundance.npz \
-    --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevaeout_dadam_reclustering \
+    --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevae_${annotator}_predictor_${thres}_reclustering \
     --hmmout_path /home/projects/cpr_10006/projects/semi_vamb/data/marker_genes/markers_sludge.hmmout \
-    --taxonomy_predictions /home/projects/cpr_10006/people/svekut/vamb/results_taxonomy_predictor_sludge.csv \
+    --taxonomy_predictions /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevae_${annotator}_predictor_${thres}/results_taxonomy_predictor.csv \
     --algorithm dbscan \
     --minfasta 200000
--- workflow_vaevae/src/longread_sludge_vamb.sh
+++ workflow_vaevae/src/longread_sludge_vamb.sh
@@ -2,23 +2,24 @@
 
 
 # vamb \
-#     --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64 \
+#     --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64_20102023 \
 #     --fasta /home/projects/cpr_10006/projects/semi_vamb/data/sludge/contigs_2kbp.fna \
 #     --rpkm /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout/abundance.npz \
 #     -l 64 \
 #     -e 500 \
-#     -q 150 \
+#     -q 25 75 150 \
 #     -o C \
 #     --cuda \
 #     --minfasta 200000
 
 vamb \
     --model reclustering \
-    --latent_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64/latent.npy \
-    --clusters_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64/vae_clusters.tsv  \
+    --latent_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64_20102023/latent.npz \
+    --clusters_path /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64_20102023/vae_clusters.tsv  \
     --fasta /home/projects/cpr_10006/projects/semi_vamb/data/sludge/contigs_2kbp.fna \
     --rpkm /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout/abundance.npz \
-    --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64_reclustering \
+    --outdir /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vambout64_20102023_reclustering \
     --hmmout_path /home/projects/cpr_10006/projects/semi_vamb/data/marker_genes/markers_sludge.hmmout \
+    --taxonomy_predictions /home/projects/cpr_10006/projects/semi_vamb/data/sludge/vaevae_full_predictor_0.5/results_taxonomy_predictor.csv \
     --algorithm dbscan \
     --minfasta 200000
