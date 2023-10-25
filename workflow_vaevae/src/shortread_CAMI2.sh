50675476704c2a0a7a4aa078959e0f77e75b21a2
Svetlana Kutuzova <galkina.s.a@gmail.com>
2023-10-25 15:08:37 +0200

    feat: interface with subcommands

--- workflow_vaevae/src/shortread_CAMI2.sh
+++ workflow_vaevae/src/shortread_CAMI2.sh
@@ -7,31 +7,30 @@ thres=$3
     # --taxonomy_predictions /home/projects/cpr_10006/people/svekut/cami2_urog_out_32_667/results_taxonomy_predictor.csv
     # --taxonomy /home/projects/cpr_10006/people/svekut/mmseq2/${dataset}_taxonomy.tsv \
 
-# vamb \
-#     --model vaevae \
-#     --outdir /home/projects/cpr_10006/people/svekut/cami2_${dataset}_${annotator}_${thres}_test \
+    # --cuda \
+
+# vamb bin vaevae \
+#     --outdir /home/projects/cpr_10006/people/svekut/cami2_${dataset}_${annotator}_${thres}_interface \
 #     --fasta /home/projects/cpr_10006/projects/vamb/data/datasets/cami2_${dataset}/contigs_2kbp.fna.gz \
 #     --rpkm /home/projects/cpr_10006/projects/vamb/data/datasets/cami2_${dataset}/abundance.npz \
 #     --taxonomy /home/projects/cpr_10006/people/svekut/04_mmseq2/taxonomy_cami_kfold/${dataset}_taxonomy_${annotator}.tsv \
 #     -l 32 \
-#     -e 300 \
+#     -e 1 \
 #     -q  \
 #     -t 1024 \
-#     -pe 100 \
+#     -pe 1 \
 #     -pq  \
 #     -pt 1024 \
 #     -pthr ${thres} \
 #     -o C \
-#     --cuda \
 #     --minfasta 200000
 
-vamb \
-    --model reclustering \
-    --latent_path /home/projects/cpr_10006/people/svekut/cami2_${dataset}_${annotator}_${thres}/vaevae_latent.npy \
-    --clusters_path /home/projects/cpr_10006/people/svekut/cami2_${dataset}_${annotator}_${thres}/vaevae_clusters.tsv  \
+vamb recluster \
+    --latent_path /home/projects/cpr_10006/people/svekut/cami2_${dataset}_${annotator}_${thres}_interface/vaevae_latent.npy \
+    --clusters_path /home/projects/cpr_10006/people/svekut/cami2_${dataset}_${annotator}_${thres}_interface/vaevae_clusters.tsv  \
     --fasta /home/projects/cpr_10006/projects/vamb/data/datasets/cami2_${dataset}/contigs_2kbp.fna.gz \
     --rpkm /home/projects/cpr_10006/projects/vamb/data/datasets/cami2_${dataset}/abundance.npz \
-    --outdir /home/projects/cpr_10006/people/svekut/cami2_${dataset}_reclustering_${annotator}_${thres}_test \
+    --outdir /home/projects/cpr_10006/people/svekut/cami2_${dataset}_reclustering_${annotator}_${thres}_interface \
     --hmmout_path /home/projects/cpr_10006/projects/semi_vamb/data/marker_genes/markers_cami_${dataset}.hmmout \
     --algorithm kmeans \
     --minfasta 200000
