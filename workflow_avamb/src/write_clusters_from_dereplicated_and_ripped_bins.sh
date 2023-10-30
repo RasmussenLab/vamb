5389c1bdf9e32d7364d8f29b91033d9e5092456e
Paupiera <38884866+Paupiera@users.noreply.github.com>
2023-10-30 11:50:55 +0100

    Update write_clusters_from_dereplicated_and_ripped_bins.sh

--- workflow_avamb/src/write_clusters_from_dereplicated_and_ripped_bins.sh
+++ workflow_avamb/src/write_clusters_from_dereplicated_and_ripped_bins.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
-
-
 while getopts "d:o:" opt; do
   case $opt in
     d) drep_dir=$OPTARG    ;;
-    o) outdir=$OPTARG    ;;
+    o) clusters_file=$OPTARG    ;;
     *) echo 'error' >&2
        exit 1
   esac
 done
-clusters_file=${outdir}/avamb/avamb_manual_drep_disjoint_clusters.tsv
+output_file=$(pwd)/${clusters_file}/avamb/avamb_manual_drep_disjoint_clusters.tsv
 echo 'creating z y v clusters from the final set of bins'
 for s in $(ls $drep_dir)
 do
@@ -19,15 +17,15 @@ then
 cd $s
 for bin in $(ls . 2> /dev/null)
 
-do  
+do
 if [[ $bin == **".fna" ]]
 then
 
 cluster_name=$(echo $bin | sed 's=.fna==g' | sed 's=.fa==g')
 
-for contig in $(grep '>' $bin | sed 's=>==g') 
+for contig in $(grep '>' $bin | sed 's=>==g')
 do
-echo -e   "$cluster_name""\t""$contig"  >> $clusters_file
+echo -e   "$cluster_name""\t""$contig"  >> $output_file
 done
 
 
@@ -35,4 +33,4 @@ fi
 done
 
 fi
-done 
+done
