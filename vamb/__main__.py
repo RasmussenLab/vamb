dba6c796e2e134a97f70374706a0c8958d22556e
Paupiera <pau.piera.linde@gmail.com>
2024-02-07 17:31:11 +0100

    Clean neighbourhoods before training

--- vamb/__main__.py
+++ vamb/__main__.py
@@ -1374,6 +1374,7 @@ def load_composition_and_abundance_and_embeddings(
                 c_i_idx = c_idx_d[c_i]
                 for c_j in cs_nn-set(c_i):
                     c_j_idx = c_idx_d[c_j]
+                    print(c_j_idx,type(neighs[c_i_idx]),neighs[c_i_idx])
                     if c_j_idx in set(neighs[c_i_idx]):
                         neighs[c_i_idx] = neighs[c_i_idx].remove(c_j_idx)
                     if c_i_idx in neighs[c_j_idx]:
