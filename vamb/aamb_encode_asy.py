a64eaafdd889419d9e170040cc79649562c254a9
Paupiera <pau.piera.linde@gmail.com>
2024-02-11 15:43:06 +0100

    add d_z_hood discr

--- vamb/aamb_encode_asy.py
+++ vamb/aamb_encode_asy.py
@@ -897,7 +897,7 @@ class AAE_ASY(nn.Module):
                 
                 optimizer_D_z_hood.step()
                 
-                print("d_z_hood_loss",d_z_hood_loss.item())
+                print("d_z_hood_loss",d_z_hood_loss.item(),"\n")
                 # parameters_changed = False
                 # for name, param in self.named_parameters():
                 #     if "hood" not in name:
