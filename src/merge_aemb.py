adc76f7f641deaa75d2edce9fd44f91dabdac229
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-03-07 10:50:24 +0100

    Improve src/merge_aemb.py
    * Use paths instead of strings
    * Check for invalid sample names

--- src/merge_aemb.py
+++ src/merge_aemb.py
@@ -1,18 +1,20 @@
 import argparse
-import os
 import numpy as np
 import sys
 from math import isinf, isnan
+from pathlib import Path
 
 parser = argparse.ArgumentParser(
     description="""Merge output files of `strobealign --aemb` to a single abundance TSV file.
-The sample names will be the basename of the paths in the input directory.""",
+The sample names will be the basenames of the paths in the input directory.""",
     formatter_class=argparse.RawDescriptionHelpFormatter,
     add_help=True,
 )
 
-parser.add_argument("input_dir", help="Path do directory of files")
-parser.add_argument("output_file", help="Path to output TSV file (must not exist)")
+parser.add_argument("input_dir", help="Path to directory of --aemb output files")
+parser.add_argument(
+    "output_file", help="Path to write output TSV file (must not exist)"
+)
 
 args = parser.parse_args()
 
@@ -23,34 +25,33 @@ def exit_with(message: str):
 
 
 # Check input directory exists
-if not os.path.isdir(args.input_dir):
-    exit_with(f"Error: Input is not an existing directory: {repr(args.input_dir)}")
+input = Path(args.input_dir)
+output = Path(args.output_file)
+
+if not input.is_dir():
+    exit_with(f"Error: Input is not an existing directory: '{input}'")
 
 # Check output file's parent is an existing directory.
-# We normalize to handle annoying cases like passing "a/" whose parent is technically
-# "a", but that is clearly not what the users mean
-output = os.path.normpath(args.output_file)
-parent = os.path.dirname(output)
-if parent != "" and not os.path.isdir(parent):
+if not output.parent.is_dir():
     exit_with(
-        f"Error: Output file cannot be created: Parent directory {repr(parent)} is not an existing directory"
+        f"Error: Output file cannot be created: Parent directory '{output.parent}' is not an existing directory"
     )
 
-if os.path.exists(output):
-    exit_with(f"Error: Output file already exists: {repr(output)}")
+if output.exists():
+    exit_with(f"Error: Output file already exists: '{output}'")
 
-# Read first file to get identifiers, and know how many rows to allocate in the matrix
-files = sorted(os.listdir(args.input_dir))
+files = sorted(input.iterdir())
 
 for file in files:
-    if "\t" in file:
-        exit_with(
-            f"Error: File name {repr(file)} contains a tab, which is not permitted in Vamb"
-        )
+    for char in ("\n", "\r", "\t", "\v"):
+        if char in file.name:
+            exit_with(
+                f"Error: File name '{file.name}' contains a char {repr(char)}, which is not permitted in Vamb"
+            )
 
 
-def exit_on_line(path: str, line: int, message: str):
-    exit_with(f'Error: {message}, in file "{path}" on line {line}')
+def exit_on_line(path: Path, line: int, message: str):
+    exit_with(f"Error: {message}, in file '{path}' on line {line}")
 
 
 # We allow an empty directory, but let's warn the user since it's an easy mistake
@@ -64,7 +65,7 @@ if len(files) == 0:
 
 # Parses an --aemb file, yielding (identifier, depth), where depth is a non-negative,
 # non-inf, non-nan float.
-def parse_lines(path):
+def parse_lines(path: Path):
     with open(path) as file:
         for lineno_minus_one, line in enumerate(file):
             line = line.rstrip()
@@ -77,6 +78,7 @@ def parse_lines(path):
                         exit_on_line(
                             path, lineno_minus_one + 1, "Found non-trailing empty line"
                         )
+                return
 
             fields = line.split("\t")
 
@@ -113,13 +115,13 @@ identifier_to_index: dict[str, int] = dict()
 # how big to make the matrix
 first_depths: list[float] = []
 identifiers: list[str] = []
-for identifier, depth in parse_lines(os.path.join(args.input_dir, files[0])):
+for identifier, depth in parse_lines(files[0]):
     length = len(identifier_to_index)
     identifier_to_index[identifier] = length
     # If the identifier has previously been seen, the dict entry will be overwritten
     if len(identifier_to_index) == length:
         exit_with(
-            f'Duplicate sequence name found in file "{files[0]}": "{identifier}"',
+            f"Duplicate sequence name found in file '{files[0]}': '{identifier}'",
         )
     first_depths.append(depth)
     identifiers.append(identifier)
@@ -134,7 +136,7 @@ del first_depths
 # Fill in the rest of the files
 for col_minus_one, file in enumerate(files[1:]):
     n_seen_identifiers = 0
-    for identifier, depth in parse_lines(os.path.join(args.input_dir, file)):
+    for identifier, depth in parse_lines(file):
         n_seen_identifiers += 1
         index = identifier_to_index.get(identifier)
 
@@ -142,7 +144,7 @@ for col_minus_one, file in enumerate(files[1:]):
         # in the first file)
         if index is None:
             exit_with(
-                f'Error: Identifier "{identifier}" found in file "{file}" '
+                f"Error: Identifier '{identifier}' found in file '{file}' "
                 "but not present in all files.",
             )
         matrix[index, col_minus_one + 1] = depth
@@ -150,19 +152,17 @@ for col_minus_one, file in enumerate(files[1:]):
     # Check that this file does not have a strict subset of identifiers from the first
     # file. After that, we know the set of identifiers is exactly the same
     if n_seen_identifiers != len(identifiers):
-        print(
-            f'Error: File "{file}" does not have all identifiers of file "{files[0]}".',
-            file=sys.stderr,
+        exit_with(
+            f"Error: File '{file}' does not have all identifiers of file '{files[0]}'."
         )
-        sys.exit(1)
 
-assert (
-    -1.0 not in matrix
-), "Matrix not full; this is a bug in the script and should never happen"
+assert -1.0 not in matrix, (
+    "Matrix not full; this is a bug in the script and should never happen"
+)
 
 with open(output, "w") as file:
     # We already checked this, but let's check it again
     assert len(matrix) == len(identifiers)
-    print("contigname", "\t".join(files), sep="\t", file=file)
+    print("contigname", "\t".join([p.name for p in files]), sep="\t", file=file)
     for identifier, row in zip(identifiers, matrix):
         print(identifier, "\t".join([str(i) for i in row]), sep="\t", file=file)
