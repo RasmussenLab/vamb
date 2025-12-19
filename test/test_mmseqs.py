f4daa7d81b0692b2b953732170df4e06a29be5a6
las02 <lasse101010@gmail.com>
2025-12-19 17:32:10 +0100

    Add temp file for the mmseqs part

--- test/test_mmseqs.py
+++ test/test_mmseqs.py
@@ -6,6 +6,7 @@ from pymmseqs.utils import run_mmseqs_command
 from pymmseqs.commands import createdb
 from vamb.taxvamb_easy import MMseqsRunner, Mmseqs
 import os
+import tempfile
 
 PARENTDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TESTDIR = Path(PARENTDIR) / "test" 
@@ -13,18 +14,21 @@ TESTDIR = Path(PARENTDIR) / "test"
 class TestDownloadDBMmseqs(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.tmpdir = TESTDIR / "tmp" 
+        cls._tmp_dir_resource = tempfile.TemporaryDirectory(prefix="mmseqs_test_")
+        cls.tmpdir_runmmseqs = Path(cls._tmp_dir_resource.name) 
+        cls.tmpdir_db = TESTDIR / "tmp" 
         cls.downloadlocation = TESTDIR / "database"
         cls.database = "Kalamari" # We use the kalmari db as it is small and takes ~1 min to download
         cls.filtered_db = TESTDIR / "database" / "filtered_db"
         if not cls.downloadlocation.exists():
             # Install the database
-            Mmseqs().installDatabase(cls.downloadlocation, cls.tmpdir, cls.database)
+            Mmseqs().installDatabase(cls.downloadlocation, cls.tmpdir_db, cls.database)
 
             # Create a smaller version of it for querying. Here filtering only for eukaryotes
             MMseqsRunner().add_arguments([
                 "filtertaxseqdb", cls.downloadlocation / cls.database, cls.filtered_db, "--taxon-list", "2759"
             ]).run()
+
 
     def test_download_db(self):
         files_which_should_exist = [
@@ -44,12 +48,13 @@ class TestDownloadDBMmseqs(unittest.TestCase):
             assert (self.downloadlocation / file).exists()
 
     def test_remove_tmp_files(self):
-        Mmseqs().removeTmpFiles(self.tmpdir, self.database)
-        assert not self.tmpdir.exists()
+        Mmseqs().removeTmpFiles(self.tmpdir_db, self.database)
+        assert not self.tmpdir_db.exists()
 
     def test_run_mmseqs(self):
         # Given test dataset assign taxonomy
         contigs = TESTDIR / "data/mmseqs.fna"
-        Mmseqs().assignTaxonomy(self.filtered_db, contigs, self.tmpdir)
+        Mmseqs().assignTaxonomy(self.filtered_db, contigs, self.tmpdir_runmmseqs)
+
 
 
